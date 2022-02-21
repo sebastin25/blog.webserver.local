c92e091b8cc4af9afabaa26dab88d1de6f0b23c3
Sebastián González Chaves <sebastin25@gmail.com>
2022-02-21 14:52:34 -0600

    Ep 45 Build a Register User Page

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,75 +1,44 @@
-[<Go Back](/README.md)
-
-# Forms and Authentication
-
-## Build a Register User Page
-
-Primero ocuparemos crear un controlador para el registro usando `php artisan make:controller RegisterController`, una vez creado el controlador agregaremos una funcion create() para retornar la vista de registro y la funcion store() que utilizaremos para guardar los datos a la DB.
-
-```php
-public function create()
-{
-     return view('register.create');
-}
-
-public function store()
-{
-    $attributes = request()->validate([
-        'name' => 'required|max:255',
-        'username' => 'required|min:3|max:255',
-        'email' => 'required|email|max:255',
-        'password' => 'required|min:7|max:255',
-    ]);
-
-    User::create($attributes);
-
-    return redirect('/');
-}
-```
-
-Ya con el controlador y las funciones que necesitaremos creadas, agregaremos las rutas
-
-```php
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
-```
-
-Seguidamente en nuestro modelo de usuario reemplazamos nuestra variable `protected $fillable[...];` por `protected $guarded = [];`para que nos permita asignar masivamente todos los atributos.
-
-Y para terminar crearemos nuestra vista `/resources/views/register/create.blade.php`, donde agregaremos `@csrf`, dentro del form, para que el middleware encargado de la protección CSRF valide el request
-
-```php
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
             <h1 class="text-center font-bold text-xl">Register!</h1>
+
             <form method="POST" action="/register" class="mt-10">
                 @csrf
+
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
                         Name
                     </label>
+
                     <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required>
                 </div>
+
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
                         Username
                     </label>
+
                     <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
                 </div>
+
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
                         Email
                     </label>
+
                     <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
                 </div>
+
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
                         Password
                     </label>
+
                     <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
                         required>
                 </div>
+
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
                         Submit
@@ -79,4 +48,3 @@ Y para terminar crearemos nuestra vista `/resources/views/register/create.blade.
         </main>
     </section>
 </x-layout>
-```
